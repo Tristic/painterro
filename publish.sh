665c86bf9c00c473cebbb08c6f324ca5012b7e64
Christian Lund <chlu@esmiley.dk>
2020-05-13 13:00:39 +0200

    1.0.43

--- publish.sh
+++ publish.sh
@@ -37,7 +37,7 @@ git commit -m "$VERSION"
 git push
 # npm publish
 
-res=`curl --user "$GH_USER:$GH_PATH" -X POST https://api.github.com/repos/${GH_REPO_USER}/${GH_REPO}/releases \
+res=`curl --user "$GH_USER" -X POST https://api.github.com/repos/${GH_REPO_USER}/${GH_REPO}/releases \
 -d "
 {
   \"tag_name\": \"v$VERSION\",
